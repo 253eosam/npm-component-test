28f59f5b8fe3a56dadc02e13cd8fc6f0fd4f47b6
Sungjun Lee <sungjun.lee@reboott.ai>
2023-12-07 14:32:39 +0900

    first publish

---
+++ dist/TestComponent.js
@@ -0,0 +1,6 @@
+// src/lib/TestComponent
+import React from "react";
+const TestComponent = () => {
+  return /*#__PURE__*/React.createElement("span", null, "\uBC30\uD3EC\uC6A9 \uCEF4\uD3EC\uB10C\uD2B8");
+};
+export default TestComponent;
---
+++ src/lib/TestComponent.jsx
@@ -0,0 +1,8 @@
+// src/lib/TestComponent
+import React from "react";
+
+const TestComponent = () => {
+  return <span>배포용 컴포넌트</span>;
+};
+
+export default TestComponent;
